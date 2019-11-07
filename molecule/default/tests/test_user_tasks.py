d4545a5d7cb04404653a8ef625a2ed0a2a4ef9fa
Manolis Mavrofidis <emavrofid@ote.gr>
2019-11-07 16:38:03 +0200

    Initial Commit

---
+++ molecule/default/tests/test_user_tasks.py
@@ -0,0 +1,31 @@
+import os
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_recon_user(host):
+    """ Assert user exists on host with the right shell
+    and in the right group. """
+    user = host.user('recon')
+
+    assert user.exists
+    assert user.shell == '/bin/bash'
+    assert user.home == '/home/recon/'
+    assert 'sudo' in user.groups
+
+
+def test_source_file(host):
+    """ Assert recon_tools directory exists in user's home folder. """
+    recon_tools = host.file('/home/recon/.recon_tools')
+
+    assert recon_tools.exists
+   
+def test_ssh_authorized_file(host):
+    """ Assert SSH authorized key exists in user's home folder. """
+    authorized_keys = host.file('/home/recon/.ssh/authorized_keys')
+
+    assert authorized_keys.exists
+    assert authorized_keys.contains('SomeTestKey')
